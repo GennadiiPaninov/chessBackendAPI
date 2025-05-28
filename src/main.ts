d7199296f84fc86cc2db2a8aa2132ca66b0c9e3c
Gennadij Lobotskij <utyf185@mail.ru>
2025-05-28 16:04:06 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import { corsMiddleware } from '../middleware/cors.middleware';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(corsMiddleware);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+
+  // можно оставить для локальной разработки
   app.enableCors({
     origin: true,
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
